47f4246191565e0d4eb7ab5d819886613b255db1
Rudraksh Raina <1819rudraksh@gmail.com>
2026-04-10 22:52:51 +0530

    feat: add positions route

--- backend/Routes/OrdersRoute.js
+++ backend/Routes/OrdersRoute.js
@@ -1,9 +1,10 @@
 const router  = require("express").Router();
 const {userVerification} = require("../Middlewares/AuthMiddlewares");
-const {getAllOrders, newOrder, getAllPositions} = require("../Controllers/OrdersController");
+const {getAllOrders, newOrder, getAllPositions, addPositions} = require("../Controllers/OrdersController");
 
 router.get("/allOrders",  userVerification, getAllOrders);
 router.post("/newOrder", userVerification, newOrder);
 router.get("/allPositions", userVerification, getAllPositions);
+router.get("/addPositions", addPositions);
 
 module.exports = router;
