c842de382a5eb453093fe3fab16e98bce998abc9
Rudraksh Raina <1819rudraksh@gmail.com>
2026-04-30 23:31:51 +0530

    chore: prepare frontend for production deployment

--- frontend/src/landing_page/login/Login.js
+++ frontend/src/landing_page/login/Login.js
@@ -35,7 +35,7 @@ const Login = () => {
         e.preventDefault();
         try{
             const {data} = await axios.post(
-                "http://localhost:3002/login",
+                `${process.env.REACT_APP_BACKEND_URL}/login`,
                 {
                     ...inputValue,
                 },
